77728ed25f08251532390266f07ee05f1df01147
Jakub Rawski <s30532@pjwstk.edu.pl>
2025-03-04 13:20:19 +0100

    Added Method GetAvarage

--- Cwiczenie1/Cwiczenie1/Program.cs
+++ Cwiczenie1/Cwiczenie1/Program.cs
@@ -1,8 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 public class Program {
-    public static void Main()
-    {
+
+
+
+    static int GetAvarage(int[] num) {
+        int a = 0;
+        for (int i = 0; i < num.Length; i++)
+        {
+            a += num[i];
+        }
+        return a/num.Length;
+    }
+    public static void Main(string[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         Console.WriteLine("Hello, World!");
+        Console.WriteLine(GetAvarage(arr));
     }
 
 
